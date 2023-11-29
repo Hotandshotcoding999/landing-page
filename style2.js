c5e0481d9592b4de3a37de6d62cc7ba01b8117a2
Hotandshotcoding999 <>
2023-11-29 12:03:07 +0530

    first commit

---
+++ style2.js
@@ -0,0 +1,9 @@
+function openNavBar(){
+    document.getElementById("sideNavigationBar")
+    .style.width = "50%";
+}
+
+function closeNavBar(){
+    document.getElementById("sideNavigationBar")
+    .style.width = "0%";
+}
